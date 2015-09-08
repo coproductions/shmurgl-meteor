d847afd23b358a60016b69828f86193b884958d4
Constantino <Coproductions@pc81.home>
2015-09-08 05:27:08 +0200

    pickdate plugin installed

--- client/templates/layout/layout.js
+++ client/templates/layout/layout.js
@@ -1,3 +1,19 @@
+Template.layout.rendered = function(){
+  console.log('layout rendered');
+  $('#testInput').pickadate();
+    $('.datepicker').pickadate();
+
+//     var $input = $('.datepicker').pickadate();
+// var picker = $input.pickadate('picker');
+// picker.on('open', function() {
+//     console.log('Opened.. and here I am!');
+// })
+// $('button').on('click', function(event) {
+//     event.stopPropagation();
+//     picker.open();
+// });
+};
+
  Template.layout.helpers({
     counter: function () {
       return Session.get('counter');
---
+++ client/templates/pickDate/pickDate.js
@@ -0,0 +1,3 @@
+Template.pickDate.rendered = function () {
+  $('.datepicker').pickadate()
+}
